7d9d0ebbcff63846d4b5ced60e1836e7b565fe9d
Anna <anna.kirsteina@gmail.com>
2024-05-16 17:03:07 +0300

    Comments

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -5,10 +5,14 @@ const SearchForm: React.FC<{ onSearch: (text: string) => void }> = (props) => {
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
-        const eneteredText = searchInputRef.current!.value;
+        const eneteredText = searchInputRef.current!.value; // ! - the connection will always be established therefore will never be null
         if (eneteredText.trim().length === 0) {
             // throw error
+            alert('Empty!');
             return;
+        } else {
+            alert(eneteredText);
+            searchInputRef.current!.value = '';
         }
         props.onSearch(eneteredText);
     };
